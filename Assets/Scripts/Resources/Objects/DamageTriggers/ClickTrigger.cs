4180d7f5cbb9c7c13c76688fe4c049abddc536d6
niclan051 <niclan052@gmail.com>
2024-09-10 16:23:17 +0300

    added resource object hit triggers + added support for custom triggers + added support for custom hit object types

---
+++ Assets/Scripts/Resources/Objects/DamageTriggers/ClickTrigger.cs
@@ -0,0 +1,19 @@
+﻿using TowerDefence.Resources.Objects.DamageTriggers.MouseUtils;
+using UnityEngine;
+
+namespace TowerDefence.Resources.Objects.DamageTriggers
+{
+    public class ClickTrigger : DamageTrigger
+    {
+        [SerializeField]
+        private MouseButton mouseButton;
+
+        private void OnMouseOver()
+        {
+            if (Input.GetMouseButtonDown(mouseButton.ToInt()))
+            {
+                Trigger();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Resources/Objects/DamageableResourceObject.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace TowerDefence.Resources.Objects
+{
+    public class DamageableResourceObject : ResourceObject
+    {
+        [SerializeField]
+        private int requiredHits;
+
+        [SerializeField]
+        private UnityEvent onHit;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            onHit.AddListener(ProcessHit);
+            onCollect.AddListener(KillYourself);
+        }
+
+        private void ProcessHit()
+        {
+            requiredHits--;
+            if (requiredHits <= 0)
+            {
+                CollectResource();
+            }
+        }
+        private void KillYourself()
+        {
+            Destroy(gameObject);
+        }
+
+        public void Hit()
+        {
+            onHit.Invoke();
+        }
+    }
+}
