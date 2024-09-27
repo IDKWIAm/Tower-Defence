317deda6d2a272997bbb14763d2e0efbc4bc1d7c
GarinIvan <143452603+GarinIvan@users.noreply.github.com>
2024-09-27 19:15:43 +0300

    Player movement & Cinemachine
    Реализовал механику перемещения и плавного поворота игрока, которая работает на геймпадах. Добавил и настроил плавную камеру.

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] private float rotationSpeed;
+    private Rigidbody2D rb;
+    private Vector3 moveDirection;
+    private Quaternion currentRotation;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentRotation = transform.rotation;
+    }
+    void Update()
+    {
+        GetInput();
+        GetRotation();
+    }
+    private void GetInput()
+    {
+        moveDirection.x = Input.GetAxisRaw("Horizontal");
+        moveDirection.y = Input.GetAxisRaw("Vertical");
+        moveDirection = moveDirection.normalized;
+    }
+    private void GetRotation()
+    {
+        if (moveDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, moveDirection);
+            currentRotation = Quaternion.RotateTowards(currentRotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+        transform.rotation = currentRotation;
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = moveDirection * speed;
+    }
+}
