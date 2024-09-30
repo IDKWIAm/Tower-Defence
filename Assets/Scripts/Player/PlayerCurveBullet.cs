9b46bdfbe3cca5e0743300431677b87466dc752b
GarinIvan <143452603+GarinIvan@users.noreply.github.com>
2024-09-30 21:40:28 +0300

    CurveShooting V.2
    Добавил стрельбу по квадратичной кривой Безье сразу с двух сторон - и слева, и справа. Немного упорядочил каталог, правильно настроил камеру и ускорил анимацию игрока до нужного значения.

--- Assets/Scripts/Player/PlayerCurveBullet.cs
+++ Assets/Scripts/Player/PlayerCurveBullet.cs
@@ -5,16 +5,18 @@ public class PlayerCurveBullet : MonoBehaviour
     public float duration;
     
     private float _elapsedTime = 0;
-    private float _bezierPoint;
     
     private Vector2 _p0;
     private Vector2 _p1;
     private Vector2 _p2;
+
     void Start()
     {
+        GenerateP0();
         GenerateP2();
         GenerateP1();
     }
+
     void Update()
     {
         if (_elapsedTime < duration)
@@ -28,23 +30,31 @@ public class PlayerCurveBullet : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    private void GenerateP0()
+    {
+        _p0 = transform.position;
+    }
+
     private void GenerateP2()
     {
         Vector2 mousePosition = Input.mousePosition;
         _p2 = Camera.main.ScreenToWorldPoint(mousePosition);
     }
+
     private void GenerateP1()
     {
-        _p0 = transform.position;
         Vector2 direction = (_p2 - _p0).normalized;
 
-        Vector2 perpendicular = new Vector2(-direction.y, direction.x);
+        float angle = Random.Range(-45f, 45f);
+        Vector2 perpendicular = Quaternion.Euler(0, 0, angle) * direction;
+
         float distance = Random.Range(1f, 3f);
-        _p1 = _p0 + (direction + perpendicular) * distance;
+        _p1 = _p0 + perpendicular * distance;
     }
+
     private Vector2 GetBezierPoint(float t, Vector2 p0, Vector2 p1, Vector2 p2)
     {
-        p0 = _p0;  p1 = _p1;  p2 = _p2;
-        return (1 - t) * (1 - t) * _p0 + 2 * (1 - t) * t * _p1 + t * t * _p2;
+        return (1 - t) * (1 - t) * p0 + 2 * (1 - t) * t * p1 + t * t * p2;
     }
 }
